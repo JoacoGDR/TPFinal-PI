4ad5b083b62258a8e14a5631b2be185b31fe0927
JoacoGDR <45571740+JoacoGDR@users.noreply.github.com>
2018-12-04 18:41:00 -0300

    Add files via upload

---
+++ procesamiento.h
@@ -0,0 +1,10 @@
+#ifndef _PROCESAMIENTO_ADT_
+#define _PROCESAMIENTO_ADT_
+#include "movsADT.h"
+#include "aeropADT.h"
+
+int procesarAerops(char * file, listADT aerops);
+int procesarMovs(char * file, listADT aerops, tMovsADT movimientos);
+
+
+#endif
