1d5d6962c74e4b036f8b6128cb825ca485d5ac86
JoacoGDR <45571740+JoacoGDR@users.noreply.github.com>
2018-12-04 23:05:14 -0300

    Update aeropADT.h

--- aeropADT.h
+++ aeropADT.h
@@ -1,12 +1,16 @@
 #ifndef _AEROPUERTOS_ADT_
 #define _AEROPUERTOS_ADT_
 #define LETRAS 'Z'-'A' + 1
+
+
 typedef struct aeropCDT * aeropADT;
-typedef struct listaCDT * listaADT;
+
 aeropADT * nuevaLista(void);
+
 void addAerop(aeropADT lista[LETRAS], char * oaci, char* denom);
+int agregarMov(aeropADT list[LETRAS], char oaci[5]);
+
 void ordenaCantMovs(aeropADT lista[LETRAS]);
-void freeLista(aeropADT lista[LETRAS]);
 
 
 #endif
