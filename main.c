849aed7cf6642e37c0c990881a1842a9aa111f37
JoacoGDR <45571740+JoacoGDR@users.noreply.github.com>
2018-12-05 01:53:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "aeropADT.h"
+#include "movsADT.h"
+#include "procesamiento.h"
 
+void query1(aeropADT lista){   // Recibe la lista ordenada por movimientos
+	
+	FILE * destino;
+	destino = fopen("movimientos_aeropuerto.csv", "wt");   // Va a crear este archivo y va a poner todo ahi
+
+	aeropADT aux = lista;
+
+	while(aux != NULL){
+		fprintf(destino, "%s | %s | %ld \n", aux->oaci, aux->denom, aux->cantMov);
+		aux = aux->next;
+	}
+
+	fclose(destino);
+}
